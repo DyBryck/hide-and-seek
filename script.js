c0801153c74f663659d369d4151ab414c4d4e7bf
Dylan Bryckaert <dylan.bryckaert@gmail.com>
2025-01-17 21:14:33 +0100

    fonction toggleView

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ gameTitle.addEventListener("animationend", () => {
     }, index * 400);
   }
 });
+
+const toggleView = (from, to) => {
+  from.classList.add("fade-out");
+
+  from.addEventListener("animationend", () => {
+    from.style.display = "none";
+    from.classList.remove("fade-out");
+
+    to.style.display = "flex";
+    to.classList.add("fade-in");
+
+    setTimeout(() => {
+      to.classList.add("visible");
+    }, 50);
+  });
+};
+
+const playButton = document.querySelector("#play-button");
+const startingPage = document.querySelector("#starting-page");
+const homePage = document.querySelector("#home-page");
+playButton.addEventListener("click", () => toggleView(startingPage, homePage));

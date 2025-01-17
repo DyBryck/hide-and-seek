6046cff138d1fdd837508caa7e25e793f76b07f9
Dylan Bryckaert <dylan.bryckaert@gmail.com>
2025-01-17 22:58:21 +0100

    input pseudo

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const entrySound = new Audio("./assets/audio/entry-sound.mp3");
-entrySound.play();
+// entrySound.play();
 
 const gameTitle = document.querySelector("#game-title");
 gameTitle.addEventListener("animationend", () => {
-  for (let index = 0; index < 3; index++) {
-    setTimeout(() => {
-      gameTitle.innerText += ".";
-    }, index * 400);
-  }
+  new TypeIt("#dots", {
+    cursor: false,
+  })
+    .type(".")
+    .pause(200)
+    .type(".")
+    .pause(200)
+    .type(".")
+    .go();
 });
 
 const toggleView = (from, to) => {
@@ -28,5 +32,14 @@ const toggleView = (from, to) => {
 
 const playButton = document.querySelector("#play-button");
 const startingPage = document.querySelector("#starting-page");
-const homePage = document.querySelector("#home-page");
+const homePage = document.querySelector("#intro-page");
 playButton.addEventListener("click", () => toggleView(startingPage, homePage));
+
+const pseudoField = document.querySelector("#pseudo");
+pseudoField.addEventListener("input", () => {
+  if (pseudoField.value.length >= 3) {
+    playButton.disabled = false;
+  } else {
+    playButton.disabled = true;
+  }
+});
